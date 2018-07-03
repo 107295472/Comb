14ac1841eabaabebbdf9b0c3822c3946663cccfe
107295472 <yintingji@gmail.com>
2018-07-03 11:14:34 +0800

    init

---
+++ Comb/Form1.cs
@@ -0,0 +1,40 @@
+﻿using Snowflake.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Comb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = MainID.GetID().ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Text = MainID.GetID().ToString();
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox1.Text!="")
+            {
+                Clipboard.SetDataObject(textBox1.Text);
+                textBox1.SelectAll();
+            }       
+        }
+    }
+}
---
+++ Snowflake/MainID.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Snowflake.Net
+{
+   public static class MainID
+    {
+        static IdWorker id;
+        static MainID()
+        {
+            id = new IdWorker(1,1);
+        }
+        [Microsoft.SqlServer.Server.SqlFunction]
+        public static long GetID()
+        {
+            return id.NextId();
+        }
+    }
+}
